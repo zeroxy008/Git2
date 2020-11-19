6d7f765212420a62c8f2e9b7928f1937360abc2d
zeroxy008 <2415031380@qq.com>
2020-11-19 21:28:16 +0800

    新增Test类

---
+++ src/com/zero/Test.java
@@ -0,0 +1,15 @@
+package com.zero;
+
+/**
+ * @version v1.0
+ * @ProjectName: git
+ * @ClassName: Test
+ * @Description: TODO(一句话描述该类的功能)
+ * @Author: zero
+ * @Date: 2020/11/19 21:21
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("git测试");
+    }
+}
