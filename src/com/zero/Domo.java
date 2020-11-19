647005928969e03474b5015d2dd34c06506e97aa
zeroxy008 <2415031380@qq.com>
2020-11-19 20:49:02 +0800

    第一次进行项目的提交

---
+++ src/com/zero/Domo.java
@@ -0,0 +1,15 @@
+package com.zero;
+
+/**
+ * @version v1.0
+ * @ProjectName: git
+ * @ClassName: Domo
+ * @Description: TODO(一句话描述该类的功能)
+ * @Author: zero
+ * @Date: 2020/11/19 20:42
+ */
+public class Domo {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
---
+++ src/com/zero/Ttt.java
@@ -0,0 +1,12 @@
+package com.zero;
+
+/**
+ * @version v1.0
+ * @ProjectName: git
+ * @ClassName: Ttt
+ * @Description: TODO(一句话描述该类的功能)
+ * @Author: zero
+ * @Date: 2020/11/19 20:46
+ */
+public class Ttt {
+}
